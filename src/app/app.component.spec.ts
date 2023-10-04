df5e81db238a8f54363d7d0ab23e1bf881704b43
Hakim Tarbet <ha.tarbet@gmail.com>
2023-10-04 15:20:27 +0100

    Remove default homepage, remove visual code config folder, change project readme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'find-a-bike'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('find-a-bike');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('find-a-bike app is running!');
-  });
 });
